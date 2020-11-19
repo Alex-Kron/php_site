c8bd17ccc1f0dc800f7c98c27da5c2a5f9285ee9
Alex Kron <alexmyxa@gmail.com>
2020-11-19 17:01:14 +0300

    Start with Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'alex_kron/streetfreakwear',
+  ),
+  'versions' => 
+  array (
+    'alex_kron/streetfreakwear' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v3.1.36',
+      'version' => '3.1.36.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd148f7ade295014fff77f89ee3d5b20d9d55451',
+    ),
+  ),
+);
---
+++ web/index.php
@@ -0,0 +1,3 @@
+<?php
+$smarty = new Smarty;
+?>
